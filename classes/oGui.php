b87a31e05ccd9891ce139f2e5612ea3135089cb8
xavier8854 <xavier@groumpf.org>
2019-03-25 17:55:06 +0100

    Deleted binaries

--- classes/oGui.php
+++ classes/oGui.php
@@ -2,7 +2,7 @@
 /**
  * oGui.php
  * 
- * Copyright 2019 Xavier Humbert <xavier@feanor>
+ * Copyright 2019 Xavier Humbert <xavier@amdh.fr>
  * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
